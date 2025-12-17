45cfe7ad09ad91ea44b80c4658fda9acdaa4261e
nelsonadagi <thinkiqbs@gmail.com>
2025-12-17 13:08:25 +0300

    updated docker compose

--- sauti-admin/src/utils/api.js
+++ sauti-admin/src/utils/api.js
@@ -5,7 +5,7 @@ import router from '@/router'
 console.log('Vite Environment Variables:', import.meta.env);
 
 const apiClient = axios.create({
-  baseURL: 'http://localhost:8000/api',
+  baseURL: import.meta.env.VITE_API_BASE_URL,
   timeout: 15000,
   headers: {
     'Content-Type': 'application/json',
