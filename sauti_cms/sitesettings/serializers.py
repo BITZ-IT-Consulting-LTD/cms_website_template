9fc47c22dfa251fcd02e32ab5cf9de3ee9c2fdbe
nelsonadagi <thinkiqbs@gmail.com>
2025-12-18 17:09:10 +0300

    updated docker compose

--- sauti_cms/sitesettings/serializers.py
+++ sauti_cms/sitesettings/serializers.py
@@ -1,8 +1,15 @@
 from rest_framework import serializers
-from .models import SiteSetting
+from .models import SiteSetting, GlobalSettings
 
+# DEPRECATED: This serializer is deprecated and will be removed in a future version.
 class SiteSettingSerializer(serializers.ModelSerializer):
     class Meta:
         model = SiteSetting
         fields = '__all__'
         read_only_fields = ('last_updated',)
+
+
+class GlobalSettingsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = GlobalSettings
+        fields = '__all__'
